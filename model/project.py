f64a9c534637569e8d78ea794c5f2ed2d07f2415
Anna <apzhadovets@gmail.com>
2019-04-07 00:13:59 +0300

    добавлен способ отображения проектов

--- model/project.py
+++ model/project.py
@@ -1,8 +1,14 @@
 class Project:
 
-    def __init__(self, name=None, status=None, change_inherit_global=False, view_status=None, description=None):
+    def __init__(self, name=None, status=None, change_inherit_global=False, view_status=None, description=None,
+                 id=None):
         self.name = name
         self.status = status
         self.change_inherit_global = change_inherit_global
         self.view_status = view_status
         self.description = description
+        self.id = id
+
+    def __repr__(self):
+        return "%s:%s:%s:%s:%s" % (self.id, self.name, self.status, self.view_status, self.description)
+
