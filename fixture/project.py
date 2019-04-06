837db272c9456a4460cba34384901b74afa49622
Anna <apzhadovets@gmail.com>
2019-04-06 21:39:02 +0300

    создана фикстура для проекта: - open_create_project - открыть страницу создания проекта - set_field_value - заполнение полей - select_from_list - выбор значения из списка - create_project - создание проекта

---
+++ fixture/project.py
@@ -0,0 +1,57 @@
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import os
+import re
+
+
+class ProjectManage:
+
+    def __init__(self, gen):
+        self.gen = gen
+
+    def open_create_project(self):
+        wd = self.gen.wd
+        if not (wd.current_url.endswith("/manage_proj_create_page.php") and len(
+                wd.find_elements_by_xpath("//input[@value='Add Project']")) > 0):
+            if not (wd.current_url.endswith("/manage_overview_page.php") or wd.current_url.endswith("/manage_proj_page.php")):
+                wd.find_element_by_link_text("Manage").click()
+            elif wd.current_url.endswith("/manage_overview_page.php") and len(
+               wd.find_elements_by_link_text("Manage Projects")) > 0:
+                wd.find_element_by_link_text("Manage Projects").click()
+            elif wd.current_url.endswith("/manage_proj_page.php") and len(
+                    wd.find_elements_by_link_text("Name")) > 0:
+                wd.find_element_by_xpath("//input[@value='Create New Project']").click()
+            wd.find_element_by_xpath("//input[@value='Add Project']")
+
+    def set_field_value(self, field_name, text):
+        wd = self.gen.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+        wd.find_element_by_name(field_name).send_keys(text)
+
+    def select_from_list(self, list_name, text):
+        wd = self.gen.wd
+        if text is not None:
+            wd.find_element_by_name(list_name).click()
+            Select(wd.find_element_by_name(list_name)).select_by_visible_text(text)
+
+    def create_project(self, name, status, change_inherit_global, view_state, description):
+        wd = self.gen.wd
+        self.open_create_project()
+        # заполняем имя проекта
+        self.set_field_value("name", name)
+        # выбираем статус проекта
+        self.select_from_list("status", status)
+        if change_inherit_global:
+            wd.find_element_by_name("inherit_global").click()
+        self.select_from_list("view_state", view_state)
+        # описание проекта
+        self.set_field_value("description", description)
+        # добавляем проект
+        wd.find_element_by_xpath("//input[@value='Add Project']")
+
+
+
