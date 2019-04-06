48e62b2f1cc88a6152c86aaad0d1d8402f8956dd
Anna <apzhadovets@gmail.com>
2019-04-06 21:36:11 +0300

    чтение логина и пароля для входа в mantis из файла настроек

--- conftest.py
+++ conftest.py
@@ -20,10 +20,11 @@ def load_config(file):
 def gen(request):
     global fixture
     browser = request.config.getoption("--browser")
-    web_conf = load_config(request.config.getoption("--settings"))["web"]
+    url = load_config(request.config.getoption("--settings"))["web"]
+    login_info = load_config(request.config.getoption("--settings"))["webadmin"]
     if fixture is None or not fixture.is_valid():
-        fixture = Generic(browser=browser, base_url=web_conf["base_url"])
-    fixture.session.ensure_login(username=web_conf["username"], password=web_conf["password"])
+        fixture = Generic(browser=browser, base_url=url["base_url"])
+    fixture.session.ensure_login(username=login_info["username"], password=login_info["password"])
     return fixture
 
 
