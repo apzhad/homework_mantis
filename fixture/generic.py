9f207fa06fdf8693996ad26e67cf82e0fddf46e9
Anna <apzhadovets@gmail.com>
2019-04-06 12:21:39 +0300

    Убраны неиспользуемые методы

--- fixture/generic.py
+++ fixture/generic.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 from fixture.session import SessionManage
-from fixture.group import GroupManage
-from fixture.contact import ContactManage
 
 
 class Generic:
@@ -21,8 +19,6 @@ class Generic:
         self.base_url = base_url
         self.wd.implicitly_wait(1)
         self.session = SessionManage(self)
-        self.group = GroupManage(self)
-        self.contact = ContactManage(self)
 
     def open_home_page(self):
         wd = self.wd
