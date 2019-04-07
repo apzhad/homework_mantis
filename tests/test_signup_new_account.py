6162817a7e6cc50c1c2344ed706ee844fe4de849
Anna <apzhadovets@gmail.com>
2019-04-07 21:45:28 +0300

    доработан тесте входа под новым пользователем:  * имя пользователя генерируется случайно  * при входе регистрируется email  * осуществляется вход под новым зарегистрированным пользователем

--- tests/test_signup_new_account.py
+++ tests/test_signup_new_account.py
@@ -1,4 +1,19 @@
+import random
+import string
+
+
+def random_username(prefix, max_length):
+    symbols = string.ascii_letters
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(max_length))])
+
+
 def test_signup_new_account(gen):
-    username = "user1"
+    username = random_username("user_", 10)
     password = "pass"
+    email = username + "@localhost"
     gen.james.ensure_user_exists(username, password)
+    gen.signup.new_user(username, password, email)
+    gen.session.login(username, password)
+    assert gen.session.is_login_as(username)
+    gen.session.logout()
+
