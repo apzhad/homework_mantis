c28981e9e85823b4f2b63f99600e2ed2b956a1ef
Anna <apzhadovets@gmail.com>
2019-04-07 16:59:03 +0300

    добавлена проверка наличия проектов перед удалением

--- tests/del_project.py
+++ tests/del_project.py
@@ -3,6 +3,8 @@ from model.project import Project
 
 
 def test_del_project(gen):
+    if gen.project.get_project_list() == 0:
+        gen.project.create_project(Project(name="name", status="release", inherit_global=False, view_status="public"))
     old_list = gen.project.get_project_list()
     del_project = random.choice(old_list)
     gen.project.delete_project(del_project)
