aa3c32f859f086ef9e9b9cb002cfca7f47edf96d
Anna <apzhadovets@gmail.com>
2019-04-07 23:53:22 +0300

    добавлен метод получения списка проектов

--- fixture/soap.py
+++ fixture/soap.py
@@ -1,5 +1,6 @@
 from suds.client import Client
 from suds import WebFault
+from model.project import Project
 
 
 class SoapManage:
@@ -13,3 +14,12 @@ class SoapManage:
             return True
         except WebFault:
             return False
+
+    def get_project_list(self, username, password):
+        client = Client("http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
+        project_data = client.service.mc_projects_get_user_accessible(username, password)
+        project_list = []
+        for data in project_data:
+            project_list.append(Project(name=data.name, status=data.status.name, view_status=data.view_state.name,
+                                        description=data.description))
+        return project_list
