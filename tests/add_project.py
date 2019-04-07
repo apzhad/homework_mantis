86285db54d85645c0b3a9c3362d41f46788d284b
Anna <apzhadovets@gmail.com>
2019-04-07 15:37:18 +0300

    тест добавления проекта со случайными данными и проверками

--- tests/add_project.py
+++ tests/add_project.py
@@ -1,6 +1,29 @@
 from model.project import Project
+import random
+import string
+import pytest
 
 
-def test_add_project(gen):
-    pr = Project(name="test", status="stable", change_inherit_global=False, view_status="public", description="sbdfvj")
-    gen.project.create_project(pr)
+def random_string(prefix, max_length):
+    symbols = string.ascii_letters + string.digits + ' '*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(max_length))])
+
+
+status = ["stable", "development", "release", "obsolete"]
+view_status = ["public", "private"]
+
+
+test_data = [Project(name=random_string("name", 10), status=random.choice(status),
+                     view_status=random.choice(view_status), description=random_string("descr", 30))
+             for i in range(5)]
+
+
+@pytest.mark.parametrize("project", test_data, ids=[repr(x) for x in test_data])
+def test_add_project(gen, project):
+    old_list = gen.project.get_project_list()
+    gen.project.create_project(project)
+    new_list = gen.project.get_project_list()
+    assert len(old_list)+1 == len(new_list)
+    old_list.append(project)
+    assert sorted(old_list, key=Project.sort_by_name) == sorted(new_list, key=Project.sort_by_name)
+
