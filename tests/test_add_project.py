f3a1773990d1ca9e10b09f84247fd2740771807f
Anna <apzhadovets@gmail.com>
2019-04-10 00:30:10 +0300

    имя пользователя и пароль перенесены в фикстуру

--- tests/test_add_project.py
+++ tests/test_add_project.py
@@ -21,12 +21,10 @@ test_data = [Project(name=random_string("name", 10), status=random.choice(status
 
 @pytest.mark.parametrize("project", test_data, ids=[repr(x) for x in test_data])
 def test_add_project(gen, project):
-    username = gen.config["webadmin"]["username"]
-    password = gen.config["webadmin"]["password"]
-    gen.session.ensure_login(username=username, password=password)
-    old_list = gen.soap.get_project_list(username, password)
+    gen.session.ensure_login()
+    old_list = gen.soap.get_project_list()
     gen.project.create_project(project)
-    new_list = gen.soap.get_project_list(username, password)
+    new_list = gen.soap.get_project_list()
     names = []
     for i in old_list:
         names.append(i.name)
--- tests/test_del_project.py
+++ tests/test_del_project.py
@@ -3,15 +3,13 @@ from model.project import Project
 
 
 def test_del_project(gen):
-    username = gen.config["webadmin"]["username"]
-    password = gen.config["webadmin"]["password"]
-    gen.session.ensure_login(username=username, password=password)
-    if gen.soap.get_project_list(username, password) == 0:
+    gen.session.ensure_login()
+    if gen.soap.get_project_list() == 0:
         gen.project.create_project(Project(name="name", status="release", inherit_global=False, view_status="public"))
-    old_list = gen.soap.get_project_list(username, password)
+    old_list = gen.soap.get_project_list()
     del_project = random.choice(old_list)
     gen.project.delete_project(del_project)
-    new_list = gen.soap.get_project_list(username, password)
+    new_list = gen.soap.get_project_list()
     assert len(old_list)-1 == len(new_list)
     old_list.remove(del_project)
     assert sorted(old_list, key=Project.sort_by_name) == sorted(new_list, key=Project.sort_by_name)
